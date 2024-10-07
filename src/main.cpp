401cd0b58eb9ed1de8ed493d85d7ee1f115f1598
alexander_bralnin <alexander.bralnin@gmail.com>
2024-10-07 23:15:03 +0300

    checked socks5 proxy

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,13 @@ int main() {
     // Путь к HLS мастер-плейлисту
     std::string playlist_url = "https://example.com/playlist.m3u8";
 
+
     // Выбор разрешения
     const std::string resolution = "Max";
 
     // Файл для сохранения загруженного стрима
     std::string output_file = "output_stream.ts";
-    network::NetworkClient network_client = network::NetworkClient();
+    network::NetworkClientSocks5 network_client = network::NetworkClientSocks5("127.0.0.1", 1080);
     // Создаем объект парсера и загрузчика
     m3u8::LiveStreamSegments parser = m3u8::LiveStreamSegments(&network_client, playlist_url, resolution);
     m3u8::StreamDownloader downloader(parser);
